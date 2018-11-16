d880757dac08ca16d2fa6524a3b31cb2ed0e2675
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-11-16 21:11:56 +0500

    Добавление файлов с шаблонами экранов

---
+++ js/render-element.js
@@ -0,0 +1,6 @@
+
+const getElementFromTemplate = (element) => element.cloneNode(true);
+  // const element = document.createElement('div');
+  // element.innerHtml = htmlString;
+  // return element;
+//console.log(getElementFromTemplate(confirmTemplate));
