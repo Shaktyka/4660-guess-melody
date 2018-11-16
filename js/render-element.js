7feda49737cfba3033f429a4c40805321bd556da
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-11-16 21:14:19 +0500

    Из index.html вырезан код templates

--- js/render-element.js
+++ js/render-element.js
@@ -1,5 +1,6 @@
 
 const getElementFromTemplate = (element) => element.cloneNode(true);
+
   // const element = document.createElement('div');
   // element.innerHtml = htmlString;
   // return element;
