bd91ed3fd8cbd168a88232c42f55b0c754112db2
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-11-18 17:56:15 +0500

    Реализован показ экрана первой игры

--- js/welcome-screen.js
+++ js/welcome-screen.js
@@ -1,6 +1,19 @@
 // Экран приветствия
-import getElementFromTemplate from './render-element.js';
 
+import getElementFromTemplate from './render-element.js';
+import renderScreenContent from './render-screen.js';
+import moduleGameGenre from './game-genre-screen.js';
+
+// Генерируем приветственный экран
 const welcomeScreen = getElementFromTemplate(`<section class="welcome"><div class="welcome__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div><button class="welcome__button"><span class="visually-hidden">Начать игру</span></button><h2 class="welcome__rules-title">Правила игры</h2><p class="welcome__text">Правила просты:</p><ul class="welcome__rules-list"><li>За 5 минут нужно ответить на все вопросы.</li><li>Можно допустить 3 ошибки.</li></ul><p class="welcome__text">Удачи!</p></section>`);
+
+// Кнопка перехода к экрану первой игры
+var playButton = welcomeScreen.querySelector(`.welcome__button`);
+
+// Вешаем listener на кнопку Play
+playButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  renderScreenContent(moduleGameGenre);
+})
 
 export default welcomeScreen;
