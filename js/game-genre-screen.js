579d8d7b669195a15373cfd7ab7f1fc4e2f56592
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-11-26 19:22:16 +0500

    Добавлен файл header.js

--- js/game-genre-screen.js
+++ js/game-genre-screen.js
@@ -3,30 +3,7 @@ import {getElementFromTemplate, renderScreenContent, backButtonClickHandler} fro
 import moduleGameArtist from './game-artist-screen.js';
 
 const moduleGameGenre = getElementFromTemplate(`<section class="game game--genre">
-    <header class="game__header">
-      <a class="game__back" href="#">
-        <span class="visually-hidden">Сыграть ещё раз</span>
-        <img class="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию">
-      </a>
-
-      <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-        <circle class="timer__line" cx="390" cy="390" r="370" style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
-      </svg>
-
-      <div class="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-        <span class="timer__mins">05</span>
-        <span class="timer__dots">:</span>
-        <span class="timer__secs">00</span>
-      </div>
-
-      <div class="game__mistakes">
-        <div class="wrong"></div>
-        <div class="wrong"></div>
-        <div class="wrong"></div>
-      </div>
-    </header>
-
-    <section class="game__screen">
+   <section class="game__screen">
       <h2 class="game__title">Выберите инди-рок треки</h2>
       <form class="game__tracks">
         <div class="track">
