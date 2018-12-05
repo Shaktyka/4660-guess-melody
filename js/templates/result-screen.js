1b5e9550711c227276fae871aba10e1857e627a6
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-12-05 23:59:00 +0500

    Вынос текста в переменные

--- js/templates/result-screen.js
+++ js/templates/result-screen.js
@@ -5,6 +5,8 @@ import getGameScreen from './game-screen.js';
 import countPoints from '../game-points.js';
 import backButtonClickHandler from './back-button-handler.js';
 
+const statistics = [4, 5, 8, 10, 11];
+
 const resultTemplate = (data) => `<section class="result">
   <div class="result__logo">
     <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83">
@@ -13,6 +15,12 @@ const resultTemplate = (data) => `<section class="result">
   <p class="result__total result__total--fail">${data.text}</p>
   <button class="result__replay" type="button">Попробовать ещё раз</button>
 </section>`;
+
+// Для успеха:
+//<p class="result__total">За 3 минуты и 25 секунд вы набрали 12 баллов (8 быстрых), совершив 3 ошибки</p>
+//<p class="result__text">Вы заняли 2 место из 10. Это лучше чем у 80% игроков</p>
+// или поражения:
+// <p class="result__total result__total--fail">Время вышло! Вы не успели отгадать все мелодии</p>
 
 const resultScreen = (state) => {
   const data = {
